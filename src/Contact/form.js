69b6afe581ae61e3bfdc0be6c8d31e2a43349c72
Olga Koplik <olga9194196@mail.ru>
2019-08-09 00:34:27 +0200

    updated Form

--- src/Contact/form.js
+++ src/Contact/form.js
@@ -20,9 +20,9 @@ const Form=()=> {
     }
     return (   
         <form onSubmit={e=>sendMessage(e)} className="contact__form">
-            <input value={inputs.name} onChange={e=>handleInputChange(e)} type="text" name="name" placeholder="name" title="Your name" maxlength="50" required/>
-            <input value={inputs.email} onChange={e=>handleInputChange(e)} type="email" name="email" placeholder="email" title="Your email" maxlength="50" required/>
-            <textarea value={inputs.message} onChange={e=>handleInputChange(e)} type="text" name="message" placeholder="message" title="Your message" maxlength="550" required/>
+            <input value={inputs.name} onChange={e=>handleInputChange(e)} type="text" name="name" placeholder="name" title="Your name" maxLength="50" required/>
+            <input value={inputs.email} onChange={e=>handleInputChange(e)} type="email" name="email" placeholder="email" title="Your email" maxLength="50" required/>
+            <textarea value={inputs.message} onChange={e=>handleInputChange(e)} type="text" name="message" placeholder="message" title="Your message" maxLength="550" required/>
             <input type="submit" value="send message"/>
         </form> 
     )
